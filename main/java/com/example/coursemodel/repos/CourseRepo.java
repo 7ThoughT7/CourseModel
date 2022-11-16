f8aed14c7f6576ddb2a1204dd28b8cd299e7a562
7ThoughT7 <80511053+7ThoughT7@users.noreply.github.com>
2022-11-16 13:14:37 +0300

    Delete main directory

--- main/java/com/example/coursemodel/repos/CourseRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.coursemodel.repos;
-
-import com.example.coursemodel.Course;
-import org.springframework.data.repository.CrudRepository;
-
-public interface CourseRepo extends CrudRepository<Course, Long> {
-
-    Course getById(Integer courseId);
-}
--- main/java/com/example/coursemodel/repos/ProfessorRepo.java
+++
@@ -1,8 +0,0 @@
-package com.example.coursemodel.repos;
-
-import com.example.coursemodel.Professor;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ProfessorRepo extends CrudRepository<Professor, Long> {
-
-}
